df3a5ec091c5b626c572120b9d6e04441a3be29a
Mohammad Faiz Alam <mohdfaizalam894@gmail.com>
2026-06-08 11:28:03 +0530

    lwaening while loop

---
+++ While.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class While {
+    public While() {
+   }
+
+   public static void main(String[] var0) {
+      for(int var1 = 1; var1 != 5; ++var1) {
+         System.out.print(var1 + " ");
+      }
+
+   }
+    
+}
