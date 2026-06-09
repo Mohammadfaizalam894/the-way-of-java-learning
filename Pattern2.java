ceb8263b249a781b73f0d3b89ff01f4df7562d96
Mohammad Faiz Alam <mohdfaizalam894@gmail.com>
2026-06-09 09:58:46 +0530

    square with stars

---
+++ Pattern2.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Pattern2 {
+    public static void main(String[] args){
+        righttringle(6);
+
+    }
+    static void righttringle(int n ){
+        for (int row =0;row<=n;row ++){
+            for ( int col = 0; col <= n; col++){
+                System.out.print("* ");
+            }
+        
+    
+    System.out.println();
+        }
+    }
+}
+
+    
+
