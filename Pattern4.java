b7b5111e1237f6500392c5f2c59ad9da80887fa6
Mohammad Faiz Alam <mohdfaizalam894@gmail.com>
2026-06-09 10:24:48 +0530

    tringle with same  no. in a row

---
+++ Pattern4.java
@@ -0,0 +1,23 @@
+
+import java.util.*;
+public class Pattern4 {
+    //row wise number printing
+    public static void main(String[] args){
+        righttringle(6);
+
+    }
+    static void righttringle(int n ){
+        for (int i =1;i<=n;i ++){
+            for ( int j = 0; j <i; j++){
+                System.out.print(i);
+            }
+        
+    
+    System.out.println();
+        }
+    }
+}
+
+    
+
+
