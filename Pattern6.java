3869756f86e75f5d9b81ef8d7495850a2ffe3cd0
Mohammad Faiz Alam <mohdfaizalam894@gmail.com>
2026-06-09 11:08:44 +0530

    code to print a vertical tringle with stars

---
+++ Pattern6.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Pattern6 {
+    public static void main(String [] args){
+        pattern5(5);
+    }
+    static void pattern5(int n){
+        for(int i =1;i<2 *n;i++){
+            int totalcolinrow = i>n?2*n-i:i;
+            for( int j =0;j<totalcolinrow;j++){
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
