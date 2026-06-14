034afa09039c39cb283856ecaf3bba98aeaedd15
Mohammad Faiz Alam <mohdfaizalam894@gmail.com>
2026-06-14 22:28:32 +0530

    added a recursion example of reverse the string

---
+++ ReverseStringRecursion.java
@@ -0,0 +1,18 @@
+public  class ReverseStringRecursion {
+
+    static void reverse(String str) {
+        if (str.length() == 0) {
+            return;
+        }
+
+        System.out.print(str.charAt(str.length() - 1));
+        reverse(str.substring(0, str.length() - 1));
+    }
+
+    public static void main(String[] args) {
+        String str = "HELLO";
+        reverse(str);
+    }
+} 
+    
+
